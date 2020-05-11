b67df512fae1905224440e577ef901d3ab2b00ac
Velcorn <iwishyouperish@gmail.com>
2020-05-11 22:34:20 +0200

    Minor text fixes

--- Blatt 2/Aufgabe 2.2.py
+++ Blatt 2/Aufgabe 2.2.py
@@ -2,11 +2,13 @@ import numpy as np
 import math
 import matplotlib.pyplot as plt
 
+# TODO: Ich weiß zwar nicht, ob die Funktion jetzt wenigstens halbwegs das tut, was die Aufgabe haben will, aber hier.
+
 img = []
 row = []
-mu, sigma = 0.0, 2.0
+mu, sigma = 0.0, 1.0
 
-# Create a row with 51 elements using the Gaussian function.
+# Creates a row with 51 elements using the Gaussian function.
 x = -5
 for i in range(51):
     f = round((1 / math.sqrt(2 * math.pi * sigma ** 2)) ** ((-(x - mu) ** 2) / 2 * sigma ** 2), 2)
@@ -16,7 +18,7 @@ for i in range(51):
     else:
         x = -5
 
-# Create an image with 100 of the previously created rows.
+# Creates an image with 100 of the previously created rows.
 for i in range(100):
     img.append(row)
 
