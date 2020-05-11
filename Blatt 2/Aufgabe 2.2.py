66c364b793bf99f988c4dacdf776792e958f41be
Velcorn <iwishyouperish@gmail.com>
2020-05-11 21:44:13 +0200

    Finished task 2.2?!

--- Blatt 2/Aufgabe 2.2.py
+++ Blatt 2/Aufgabe 2.2.py
@@ -1,6 +1,27 @@
 import numpy as np
 import math
-import scipy.stats
 import matplotlib.pyplot as plt
 
+img = []
+row = []
+mu, sigma = 0.0, 2.0
 
+# Create a row with 51 elements using the Gaussian function.
+x = -5
+for i in range(51):
+    f = round((1 / math.sqrt(2 * math.pi * sigma ** 2)) ** ((-(x - mu) ** 2) / 2 * sigma ** 2), 2)
+    row.append(f)
+    if x < 5:
+        x += 10 / 51
+    else:
+        x = -5
+
+# Create an image with 100 of the previously created rows.
+for i in range(100):
+    img.append(row)
+
+plt.imshow(np.clip(img, 0, 255), cmap="gray")
+plt.show()
+
+# Eine Änderung von mu bewirkt eine Verschiebung auf der x-Achse,
+# eine Änderung von sigma bewirkt eine Stauchung.
