8ec08d18035073d0512c3ff4eade0bedc0c43643
Velcorn <iwishyouperish@gmail.com>
2020-05-11 14:37:20 +0200

    Finished task 2.5

--- Blatt 2/Aufgabe 2.1.py
+++ Blatt 2/Aufgabe 2.1.py
@@ -1,11 +1,12 @@
 from skimage.io import imread
 import matplotlib.pyplot as plt
 
+# Reads in Red and NIR band as float, calculates the NDVI from the formula and shows the image.
 Red = imread("band3.png").astype(float)
 NIR = imread("band4.png").astype(float)
 NDVI = (NIR - Red)/(NIR + Red)
 plt.imshow(NDVI, cmap="gray")
 plt.show()
 
-# Ein NDVI von ~0,5 +/- an den grünen Punkten deutet auf eine Vegetationsbedeckung mit grünen Pflanzen hin.
+# Ein NDVI von ~0,5 an den grünen Punkten deutet auf eine Vegetationsbedeckung mit grünen Pflanzen hin.
 # Die Abstufungen der Grautöne lassen uns auf die Dichte der Vegatation schließen.
