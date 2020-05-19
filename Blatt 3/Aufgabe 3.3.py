3f755d256a7698ac1d6eeb2842da29b80d6ed850
Velcorn <iwishyouperish@gmail.com>
2020-05-19 03:39:59 +0200

    Fuck my life, it's almost 4 am.

--- Blatt 3/Aufgabe 3.3.py
+++ Blatt 3/Aufgabe 3.3.py
@@ -18,33 +18,46 @@ plt.imshow(binary, cmap="gray")
 
 # 3.
 # a)
-coords = np.argwhere(binary == 1)
-
+coords = list(map(list, zip(*np.nonzero(binary))))
+print(coords)
 
 # b)
+count = 0
+visited = []
+
+
 # Get the neighboring pixels of a pixel.
 def get_neighbors(coord):
     potential_neighbors = [[0, -1], [1, -1], [1, 0], [1, 1], [0, 1], [-1, 1], [-1, 0], [-1, -1]]
     reachable_neighbors = []
     for pn in potential_neighbors:
         new_coord = [coord[0] + pn[0], coord[1] + pn[1]]
-        if new_coord[0] in range(binary.shape[0]) and new_coord[1] in range(binary.shape[1]):
+        if new_coord not in visited:
             reachable_neighbors += [new_coord]
 
     return reachable_neighbors
 
 
 # Main function to count the number of errors in an image.
-def count_errors(count):
-    visited = {}
-    queue = [[coords[0], coords[1]]]
-
+def count_errors(count, coords, visited):
+    queue = [coords[0]]
     while queue:
-        pixel = queue.pop()
+        pixel = queue[0]
+        print(pixel)
         for n in get_neighbors(pixel):
-            if [n[0], n[1]] not in visited:
-                queue.append([n[0], n[1]])
-        visited[x] = pixel
+            print(n)
+            if n in coords and n not in visited:
+                queue.append(n)
+                # print(queue)
+        visited.append([pixel, count])
+        del(queue[0])
+        index = coords.index(pixel)
+        del(coords[index])
+    count += 1
+    print(count)
 
-    count_errors(count)
+    count_errors(count, coords, visited)
     return count
+
+
+print(count_errors(0, coords, visited))
