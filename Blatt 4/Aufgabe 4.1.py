445e7b26fb7b19f6526d1c4ed8dc289ef349d721
Velcorn <iwishyouperish@gmail.com>
2020-05-25 18:41:03 +0200

    Aufgabenblatt 4, Aufgaben 4.1 und 4.4

---
+++ Blatt 4/Aufgabe 4.1.py
@@ -0,0 +1,28 @@
+import numpy as np
+from skimage.io import ImageCollection
+import matplotlib.pyplot as plt
+
+imgs = ImageCollection("bild*.png")
+
+# 1.
+# Gamma correction: "Aufhellen" des relevanten Bildbereichs.
+gamma = 1.5
+img1 = np.power(imgs[0], gamma)
+plt.imshow(img1, cmap="gray", vmin=0, vmax=255)
+plt.show()
+# Es sind 8 Poller (den halbwegs verdeckten mitgezählt)
+
+# 2.
+# Contrast stretching: Differenzierung der Farben.
+minimum = np.min(imgs[1])
+maximum = np.max(imgs[1])
+img2 = np.round((imgs[1] - minimum) / (maximum - minimum) * 255)
+plt.imshow(img2, cmap="gray")
+plt.show()
+
+# 3.
+# Intensity-level slicing: Hervorheben eines Farbbereichs.
+img3 = imgs[2]
+img3[np.logical_and(imgs[2] > 100, imgs[2] < 140)] = 255
+plt.imshow(img3, cmap="gray")
+plt.show()
---
+++ Blatt 4/Aufgabe 4.4.py
@@ -0,0 +1,35 @@
+import numpy as np
+from skimage.io import imread
+import matplotlib.pyplot as plt
+
+tv = imread("tv.png")
+
+
+# 1.
+def scale(img, factor):
+    # Calculate new image shape and create new image with it.
+    size = (int(img.shape[0] * factor), int(img.shape[1] * factor))
+    scaled_img = np.zeros(size)
+
+    # Iterate over all pixels and set their values based on the input image.
+    for x in range(size[0]):
+        for y in range(size[1]):
+            scaled_img[x, y] = img[int(x / factor), int(y / factor)]
+
+    return scaled_img
+
+
+# 2.
+plt.imshow(scale(tv, 2), cmap="gray")
+plt.show()
+
+# 3.
+# Sieht nicht danach aus. Vorausgesetzt die Funktion ist richtig implementiert ^^
+
+# 4.
+half_size = scale(tv, 0.5)
+original_size = scale(half_size, 2)
+plt.imshow(original_size, cmap="gray")
+plt.show()
+# Nein, da beim Verkleinern Informationen verloren gehen,
+# die beim Vergrößern nicht zurückgewonnen werden.
