3aa82cc5dabecab660bcdff6bf896322325e3a57
Velcorn <iwishyouperish@gmail.com>
2020-06-03 00:34:04 +0200

    Close enough, I guess...

--- Blatt 5/Aufgabe 5.3.py
+++ Blatt 5/Aufgabe 5.3.py
@@ -1,20 +1,21 @@
 import numpy as np
 from skimage.io import imread
+from skimage.color import rgb2hsv, hsv2rgb
 import matplotlib.pyplot as plt
 from math import acos, cos, sqrt
 
 mandrill = imread("mandrillFarbe.png")
 
 
-def rgb_to_cmy(img):
+def rgb2cmy(img):
     return np.subtract(1, np.divide(img, 255))
 
 
-def cmy_to_rgb(img):
+def cmy2rgb(img):
     return np.multiply(np.subtract(1, img), 255).astype(int)
 
 
-def rgb_to_hsi(img):
+def rgb2hsi(img):
     # Normalize RGB values.
     img = np.divide(img, 255)
 
@@ -26,11 +27,9 @@ def rgb_to_hsi(img):
             g = px[1]
             b = px[2]
 
-            theta = acos(0.5 * ((r - g) + (r - b)) / (sqrt((r - g) ** 2 + (r - b) * (g - b)) + 0.0001))
-            if b <= g:
-                h = theta
-            else:
-                h = 360 - theta
+            h = acos(0.5 * ((r - g) + (r - b)) / (sqrt(((r - g) ** 2 + (r - b) * (g - b))) + 0.0001))
+            if b > g:
+                h = 360 - h
             h /= 360
             s = 1 - 3 / (r + b + g) * min(r, g, b)
             i = 1 / 3 * (r + g + b)
@@ -40,7 +39,7 @@ def rgb_to_hsi(img):
     return img
 
 
-def hsi_to_rgb(img):
+def hsi2rgb(img):
     # Calculate RGB values for each pixel based on the formulas.
     for x in range(img.shape[1]-1):
         for y in range(img.shape[0]-1):
@@ -52,17 +51,17 @@ def hsi_to_rgb(img):
 
             if 0 <= h <= 120:
                 b = i * (1 - s)
-                r = i * (1 + s * cos(h) / cos(60 - h))
+                r = i * abs(1 + s * cos(h) / cos(60 - h))
                 g = 3 * i - (r + b)
             elif 120 <= h <= 240:
                 h -= 120
                 r = i * (1 - s)
-                g = (1 + s * cos(h) / cos(60 - h))
+                g = i * abs(1 + s * cos(h) / cos(60 - h))
                 b = 3 * i - (r + g)
             else:
                 h -= 240
                 g = i * (1 - s)
-                b = (1 + s * cos(h) / cos(60 - h))
+                b = i * abs(1 + s * cos(h) / cos(60 - h))
                 r = 3 * i - (g + b)
 
             img[y, x] = r, g, b
@@ -71,17 +70,17 @@ def hsi_to_rgb(img):
 
 
 # RGB to CMY
-plt.imshow(rgb_to_cmy(mandrill))
+plt.imshow(rgb2cmy(mandrill))
 # plt.show()
 
 # CMY to RGB
-plt.imshow(cmy_to_rgb(rgb_to_cmy(mandrill)))
+plt.imshow(cmy2rgb(rgb2cmy(mandrill)))
 # plt.show()
 
 # RGB to HSI
-# plt.imshow(rgb_to_hsi(mandrill))
+plt.imshow(rgb2hsi(mandrill))
 # plt.show()
 
 # HSI to RGB
-plt.imshow(hsi_to_rgb(rgb_to_hsi(mandrill)))
+plt.imshow(hsv2rgb(rgb2hsi(mandrill)))
 plt.show()
