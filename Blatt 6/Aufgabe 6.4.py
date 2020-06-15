c5cd03a2f6c7a238f6fd86aa0f97a53652192f19
velcorn <iwishyouperish@gmail.com>
2020-06-15 20:39:52 +0200

    Start 6.4, fuck that though.

--- Blatt 6/Aufgabe 6.4.py
+++ Blatt 6/Aufgabe 6.4.py
@@ -3,4 +3,38 @@ from skimage.io import imread
 import matplotlib.pyplot as plt
 import time
 
+# 1.
+bild1 = imread("bild1.png")
+bild2 = imread("bild2.png")
 
+hist1 = np.histogram(bild1, bins=256, range=(0, 256), density=True)
+hist2 = np.histogram(bild2, bins=256, range=(0, 256), density=True)
+
+'''plt.figure(1)
+plt.hist(bild1.flatten(), bins=256, range=(0, 256), density=True)
+plt.show()
+plt.figure(2)
+plt.hist(bild2.flatten(), bins=256, range=(0, 256), density=True)
+plt.show()'''
+
+
+# 2.
+def equalize_hist(hist):
+    print(hist[0])
+    t = []
+    val = 0
+    for x in range(len(hist[0])):
+        val += 255 * hist[0][x]
+        t.append(int(val))
+
+    a = np.asarray(t)
+
+    return a
+
+
+plt.figure(3)
+plt.hist(equalize_hist(hist1), bins=256, range=(0, 256), density=True)
+plt.show()
+
+plt.imshow(equalize_hist(hist1), vmin=0, vmax=255)
+plt.show()
